12c2e9a3f205a3806f720a2766448e98508dc486
Manohar-1 <mudhapakamanohar@gmail.com>
2026-07-27 19:39:45 +0530

    added local grammar

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import HTMLResponse
+
+from pydantic import BaseModel
+
 import os 
 import markdown 
+import language_tool_python
 
-from fastapi.responses import HTMLResponse
+
+class GrammarRequest(BaseModel):
+    text: str
+
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
+
+    
+    # matches = tool.check(text)
+    # match = matches[0]
+
+    # print(match.message)
+    # print(match.replacements)
+    # print(match.rule_id)
+    # print(match.matched_text)
+
+    
+
     return {"message": "Welcome to the FastAPI Note Taking Application!"}
 
 
@@ -61,9 +82,27 @@ def delete_note(note_name:str):
         return {"message": f"{note_name} deleted successfully!"}
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Note not found")
+
+
+@app.post("/grammar-check")
+def grammar_check(request: GrammarRequest):
+    errors = check_grammar(request.text)
+    return {"original_text": request.text, "error_count": len(errors), "errors": errors}
+
+def check_grammar(text: str):
+    errors = []
+    tool = language_tool_python.LanguageTool('en-US')
     
+    matches = tool.check(text)
+    for match in matches:
 
-
+        errors.append({
+            "message": match.message,
+            "replacements": match.replacements,
+            "rule_id": match.rule_id,
+            "matched_text": match.matched_text
+        })
+    return errors
 
 
 
